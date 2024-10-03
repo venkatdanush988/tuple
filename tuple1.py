d1aede35bc1e711fd21ddf331744a9b16fc8a68e
venkatdanush988 <22hr1a3205@mtieat.org>
2024-10-03 13:49:34 +0530

    Create tuple1.py

---
+++ tuple1.py
@@ -0,0 +1,19 @@
+"""
+Tuple
+1) Write a program to get the tuple elements and print it.
+Sample Input:
+3
+20
+10
+30
+Sample Output:
+(20, 10, 30)
+
+"""
+
+lst=[]
+n=int(input())
+for i in range(0,n):
+    ele=int(input())
+    lst.append(ele)
+print(tuple(lst))
