acbbd3779d43744da6136d12a34838e379cab071
venkatdanush988 <22hr1a3205@mtieat.org>
2024-10-03 13:50:21 +0530

    Create tuple2.py

---
+++ tuple2.py
@@ -0,0 +1,11 @@
+"""
+2) Write a program to get the elements of tuple in a single line separated by space and print the tuple values.
+Sample Input:
+20 10 30
+Sample Output:
+20, 10, 30
+
+"""
+a=map(int,input().split())
+b=tuple(a)
+print(b)
